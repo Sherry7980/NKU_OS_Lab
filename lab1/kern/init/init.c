e6f88a62583f8432a54dacef0d4b00454f3516f9
G1 ReqChen23 <2310711@nankai.edu.cn>
2025-10-10 15:39:15 +0800

    代码注释

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <sbi.h>
-int kern_init(void) __attribute__((noreturn));
+//OS内核初始化函数
+int kern_init(void) __attribute__((noreturn)); //不返回
 
-int kern_init(void) {
-    extern char edata[], end[];
-    memset(edata, 0, end - edata);
+int kern_init(void) { 
+    // edata: BSS段的开始地址（未初始化数据段）
+    // end:   程序结束地址（BSS段的结束地址）
+    extern char edata[], end[]; //外部变量
+    memset(edata, 0, end - edata);  //清零BSS段：将edata到end之间的内存区域初始化为0
 
-    const char *message = "(THU.CST) os is loading ...\n";
-    cprintf("%s\n\n", message);
-   while (1)
+    const char *message = "(THU.CST) os is loading ...\n"; //定义启动消息
+    cprintf("%s\n\n", message); //使用控制台输出函数打印启动消息，cprintf是之前介绍过的格式化输出函数
+   while (1) //无限循环
         ;
 }
--- lab1/kern/libs/stdio.c
+++ lab1/kern/libs/stdio.c
@@ -8,7 +8,7 @@
  * cputch - writes a single character @c to stdout, and it will
  * increace the value of counter pointed by @cnt.
  * */
-static void cputch(int c, int *cnt) {
+static void cputch(int c, int *cnt) { //向标准输出写入单个字符，并递增字符计数器
     cons_putc(c);
     (*cnt)++;
 }
@@ -22,7 +22,7 @@ static void cputch(int c, int *cnt) {
  * Call this function if you are already dealing with a va_list.
  * Or you probably want cprintf() instead.
  * */
-int vcprintf(const char *fmt, va_list ap) {
+int vcprintf(const char *fmt, va_list ap) { //格式化字符串并输出到标准输出（可变参数版本）
     int cnt = 0;
     vprintfmt((void *)cputch, &cnt, fmt, ap);
     return cnt;
@@ -34,7 +34,7 @@ int vcprintf(const char *fmt, va_list ap) {
  * The return value is the number of characters which would be
  * written to stdout.
  * */
-int cprintf(const char *fmt, ...) {
+int cprintf(const char *fmt, ...) { //格式化字符串并输出到标准输出（主要接口）
     va_list ap;
     int cnt;
     va_start(ap, fmt);
@@ -44,13 +44,13 @@ int cprintf(const char *fmt, ...) {
 }
 
 /* cputchar - writes a single character to stdout */
-void cputchar(int c) { cons_putc(c); }
+void cputchar(int c) { cons_putc(c); } //向标准输出写入单个字符的简化接口
 
 /* *
  * cputs- writes the string pointed by @str to stdout and
  * appends a newline character.
  * */
-int cputs(const char *str) {
+int cputs(const char *str) { //输出字符串到标准输出，并自动追加换行符
     int cnt = 0;
     char c;
     while ((c = *str++) != '\0') {
@@ -61,7 +61,7 @@ int cputs(const char *str) {
 }
 
 /* getchar - reads a single non-zero character from stdin */
-int getchar(void) {
+int getchar(void) { //从标准输入读取单个非零字符
     int c;
     while ((c = cons_getc()) == 0) /* do nothing */;
     return c;
