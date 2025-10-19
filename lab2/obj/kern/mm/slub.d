460446c97f5cea514bd2cf591046d88772d2a8c0
G1 ReqChen23 <2310711@nankai.edu.cn>
2025-10-19 14:56:19 +0800

    lab2 small changes

---
+++ lab2/obj/kern/mm/slub.d
@@ -0,0 +1,4 @@
+obj/kern/mm/slub.o obj/kern/mm/slub.d: kern/mm/slub.c libs/defs.h \
+ libs/stdio.h libs/stdarg.h libs/string.h kern/debug/assert.h \
+ kern/mm/pmm.h kern/mm/memlayout.h libs/list.h kern/mm/mmu.h libs/riscv.h \
+ kern/mm/slub.h
---
+++ lab2/obj/kern/mm/slub_tests.d
@@ -0,0 +1,4 @@
+obj/kern/mm/slub_tests.o obj/kern/mm/slub_tests.d: kern/mm/slub_tests.c \
+ libs/defs.h libs/stdio.h libs/stdarg.h kern/debug/assert.h libs/string.h \
+ kern/mm/pmm.h kern/mm/memlayout.h libs/list.h kern/mm/mmu.h libs/riscv.h \
+ kern/mm/slub.h
